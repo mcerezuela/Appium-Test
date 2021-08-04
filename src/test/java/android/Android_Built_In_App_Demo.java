efcbda06f94cbf07532e84ccc5c5cbe978d5e41d
marcusalten <marcus.cerezuela@alten.es>
2021-08-04 10:49:42 +0200

    Added Test for Calculator Built in App

--- src/test/java/android/Android_Built_In_App_Demo.java
+++ src/test/java/android/Android_Built_In_App_Demo.java
@@ -1,8 +1,10 @@
-package Android;
+package android;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -10,26 +12,30 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class FirstAndroidTest {
+public class Android_Built_In_App_Demo {
 
     AppiumDriver driver;
 
     @BeforeTest
     public void setUp() throws MalformedURLException {
+
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
-        caps.setCapability("automationName", "espresso");
-        caps.setCapability("deviceName", "emulator-5554");
+        caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("platformVersion", "5.1");
-        caps.setCapability("forceEspressoRebuild",true);
-        caps.setCapability("app", System.getProperty("user.dir") + "/apps/ApiDemos.apk");
+        caps.setCapability("deviceName", "emulator-5554");
+        caps.setCapability("appPackage", "com.android.calculator2");
+        caps.setCapability("appActivity", ".Calculator");
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
     }
 
     @Test
-    public void click_App_Button() {
-        // code
-        driver.findElementByAccessibilityId("App").click();
+    public void click_test() {
+        driver.findElement(By.id("digit_1")).click();
+        driver.findElement(By.id("op_add")).click();
+        driver.findElement(By.id("digit_3")).click();
+        driver.findElement(By.id("eq")).click();
+        Assert.assertEquals(driver.findElement(By.id("formula")).getText(),"4");
     }
 
     @AfterTest
@@ -38,5 +44,4 @@ public class FirstAndroidTest {
             driver.quit();
         }
     }
-
 }
